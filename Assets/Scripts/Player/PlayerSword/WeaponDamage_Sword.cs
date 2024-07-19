9346bfa0ee6d04650f255b540ef91184c816a3ef
KuBonJae <qhswo9@gmail.com>
2024-07-19 10:25:57 +0900

    [feat] 근거리 공격 패턴 및 칼로 총탄 지우기 기능 추가

--- Assets/Scripts/Player/PlayerSword/WeaponDamage_Sword.cs
+++ Assets/Scripts/Player/PlayerSword/WeaponDamage_Sword.cs
@@ -31,10 +31,10 @@ public class WeaponDamage_Sword : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))// || collision.gameObject.CompareTag("Boss"))
+        if (collision.gameObject.CompareTag("EnemyWeapon"))// || collision.gameObject.CompareTag("Boss"))
         {
             // reduce enemy's hp or kill them
-            //Destroy(collision.gameObject);
+            Destroy(collision.gameObject);
         }
     }
 }
